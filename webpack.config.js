3a2e1b74ba6f500b62ebf05d6d7c780f30f1f413
Cesar Valencia <cesar4a6z@gmail.com>
2023-09-12 15:01:49 -0500

    Impelemt Google Maps Searching

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const Dotenv = require('dotenv-webpack')
 
 module.exports = {
   mode: 'development',
@@ -26,5 +27,13 @@ module.exports = {
   },
   resolve: {
     extensions: ['.ts', '.js']
-  }
+  },
+  plugins: [
+    new Dotenv({
+      path: './.env',
+      safe: true,
+      defaults:true,
+      systemvars:true
+    })
+  ]
 };
